fb873c8456d1ca43d32810401720c0a10c237ab7
Rubyleaf-MBarton <mbarton@rubyleaf.co.uk>
2016-12-12 03:14:52 +0000

    init: angular skeleton

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name projectFolderApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the projectFolderApp
+ */
+angular.module('projectFolderApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name projectFolderApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the projectFolderApp
+ */
+angular.module('projectFolderApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
